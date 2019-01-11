9b9bc1a55975cb4ed6fcab1fb20cbf43e949c6fa
park dong hyun <pdh6547@gmail.com>
2019-01-11 17:35:35 +0900

    Create Day3

--- Day3/community/src/main/java/com/community/controller/BookController.java
+++ Day3/community/src/main/java/com/community/controller/BookController.java
@@ -14,8 +14,17 @@ public class BookController {
 
     @GetMapping("/books")
     public String getBookList(Model model) {
+        //bookList는 property 이다.
+        //bookList는 book이라는 객체를 포함하고있다.
         model.addAttribute("bookList", bookService.getBookList());
+        model.addAttribute("bookList2", bookService.getBookList());
+
         //book은 view 이름
+        return "book";
+    }
+    @GetMapping("/books2")
+    public String getInteger(Model model){
+        model.addAttribute("bookList3", bookService.getInteger());
         return "book";
     }
 }
